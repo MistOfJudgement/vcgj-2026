6273973c60598e9d36bbb3e378a380012131b4f1
VernalUmbrella <vernalumbrella@gmail.com>
2026-03-29 01:51:25 -0400

    feat: include some assets

--- src/Player.gd
+++ src/Player.gd
@@ -2,21 +2,23 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed: float = 250
+@onready var sprite: Sprite2D = $Sprite2D
 # I know godot has an input system let me code quick
 # fix later do now
+
 
 func _process(delta: float) -> void:
 	var vel: Vector2 = Vector2.ZERO
 	if Input.is_key_pressed(KEY_A):
 		vel += Vector2.LEFT
+		sprite.flip_h = true
 	if Input.is_key_pressed(KEY_D):
 		vel += Vector2.RIGHT
+		sprite.flip_h = false
 	if Input.is_key_pressed(KEY_W):
 		vel += Vector2.UP
 	if Input.is_key_pressed(KEY_S):
 		vel += Vector2.DOWN
-	
+
 	velocity = vel.normalized() * speed
 	move_and_slide()
-	
-		
